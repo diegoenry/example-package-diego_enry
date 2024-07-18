66162362f70559669cc06cbdb04cfc25f4611132
DIEGO ENRY GOMES <dgomes@live.com>
2024-07-18 14:28:11 +0000

    feat: Added a print statement to rescale

--- src/example_package_diego_enry/rescale.py
+++ src/example_package_diego_enry/rescale.py
@@ -7,6 +7,7 @@ def rescale(input_array):
     Takes an array as input, and returns a corresponding array scaled so that 0
     corresponds to the minimum and 1 to the maximum value of the input array.
     """
+    print("Rescaling")
     low = np.min(input_array)
     high = np.max(input_array)
     output_array = (input_array - low) / (high - low)
